eefdcb620c90c43008b29840a439909e5f4d3ca4
Bhavya Goyal <bhavya20bcs104@iiitkottayam.ac.in>
2021-07-16 21:46:01 +0530

    added bs

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const navLinks = document.getElementsByClassName('nav-link');
 
+console.log(navLinks)
 
+const changeActive = (event) => { 
 
+    for(let i = 0;i<navLinks.length;i++)
+    {
+        if(navLinks[i].classList[1] === 'active')
+        {
+            navLinks[i].classList.remove('active');
+        }
+    }
+
+    if(event.target.classList.length === 1)
+        event.target.classList.add('active');
+
+}
+
+for(let i = 0;i<navLinks.length;i++)
+{
+    navLinks[i].addEventListener('click', changeActive);
+}
 
 
 
