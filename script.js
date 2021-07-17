eec667191c05b51fa7dfa73873014c304ed7619d
Bhavya Goyal <bhavya20bcs104@iiitkottayam.ac.in>
2021-07-17 11:50:09 +0530

    saving

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 const dropdowns = document.getElementsByClassName('dropdown');
 const y = document.body.offsetWidth;
-console.log(y);
 
-for(let i =0; i < dropdowns.length;i++)
-{
-    dropdowns[i].addEventListener('click', () => {
-
-        for(let j =0; j < dropdowns.length;j++)
-        {
-            if(dropdowns[j].childNodes[3].classList[1] === 'show' && j !== i)
-            {
-                console.log("run");
-                dropdowns[j].childNodes[3].click();
-            }
-        }
-
-        dropdowns[i].childNodes[3].style.left = `-${(document.documentElement.scrollWidth - window.innerWidth)*2}px`;
-    })
-}
+dropdowns[0].childNodes[3].style.left = `-212px`;
+dropdowns[1].childNodes[3].style.left = `-50px`;
+dropdowns[2].childNodes[3].style.left = `-70px`;
 
 
