0a787a88c762489a27a08eee105ec96278d254f6
Bhavya Goyal <bhavya20bcs104@iiitkottayam.ac.in>
2021-07-17 17:49:44 +0530

    header(done)

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ dropdowns[0].childNodes[3].style.left = `-212px`;
 dropdowns[1].childNodes[3].style.left = `-50px`;
 dropdowns[2].childNodes[3].style.left = `-70px`;
 
+for(let i = 0; i<3;i++)
+{
+    dropdowns[i].childNodes[1].addEventListener('click',() => dropdowns[i].scrollIntoView(true))
+}
 
