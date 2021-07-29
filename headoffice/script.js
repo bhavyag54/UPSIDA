eabc1ec456ea54295bbba6c89a783e01a1349854
bhavya goyal <bhavyagoyal54@gmail.com>
2021-07-29 21:13:26 +0530

    Linking done

--- headoffice/script.js
+++ headoffice/script.js
@@ -2,16 +2,9 @@
 const dropdowns = document.getElementsByClassName('dropdown');
 const y = document.body.offsetWidth;
 
-dropdowns[0].childNodes[3].style.left = `-212px`;
+dropdowns[0].childNodes[3].style.left = `-0px`;
 dropdowns[1].childNodes[3].style.left = `-50px`;
 dropdowns[2].childNodes[3].style.left = `-130px`;
-
-for(let i = 0; i<3;i++)
-{
-    dropdowns[i].childNodes[1].addEventListener('click',() => dropdowns[i].scrollIntoView(true))
-}
-
-
 
 //imp
 
