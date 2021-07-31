f98afcd4c42c146c7c680032ad9156d35ff17ec0
bhavya goyal <bhavyagoyal54@gmail.com>
2021-07-31 18:15:54 +0530

    added media querys to e-services

---
+++ e-services/script.js
@@ -0,0 +1,12 @@
+const today = document.getElementById('date');
+const tim = document.getElementById('time');
+
+today.innerText = new Date().toDateString();
+tim.innerText = new Date().toTimeString().slice(0,9);
+
+setInterval(() => {
+
+    today.innerText = new Date().toDateString();
+    tim.innerText = new Date().toTimeString().slice(0,9);
+
+},1000);
